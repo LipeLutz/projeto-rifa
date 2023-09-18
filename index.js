aaae6ac6923aacaf2913f9eae99565f552add6fd
Felipe Lutz <felipeclutz@gmail.com>
2023-09-17 22:57:50 -0300

    Mudanças estruturais
    Mudei o layout e adicionei a funcionalidade de sortear um participante.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const guardarLocalStorage = (client) => localStorage.setItem(`rifa`, JSON.string
 const pegarLocalStorage = () => JSON.parse(localStorage.getItem('rifa')) ?? []
 
 const td = document.querySelectorAll('td')
-const btnSorteio = document.getElementById('btnSorteio')
+const btnSorteio = document.getElementById('btn-sorteio')
+const btnSortearNovamente = document.getElementById('sortearNovamente')
 
 const limpaInputs = () =>{
     document.getElementById('inputNome').value = ''
@@ -14,7 +15,6 @@ const colorTable = () =>{
     const dbClient = pegarLocalStorage()
 
     dbClient.map((arr) =>{
-        console.log(arr)
         td.item(arr.numero - 1).style.backgroundColor = '#49a09d'
     })
 }
@@ -30,11 +30,6 @@ const jaExisteCadastro = (client) =>{
             //alert('Este número já foi cadastrado')
             client.numero.pop()
         } 
-        /*else{
-            guardarLocalStorage(dbClient)
-        }
-            //return alert('Tudo certo')
-        */
     })
 }
 
@@ -65,16 +60,61 @@ const createClient = (client) =>{
     limpaInputs() 
 }
 
-colorTable()
+const hundred = () =>{
+    const dbClient = pegarLocalStorage()
+    if(dbClient.length == 10){
+        document.getElementById('inputNumero').setAttribute('readonly', true)
+        document.getElementById('inputNome').setAttribute('readonly', true)
 
-/*
+        return true
+    }
+        return false
+}
+
+const sorteioVencedor = (randomNumber) =>{
+    document.getElementById('loadingParticipante').style.display = 'block'
+    document.getElementById('main').style.display = 'none'
+    console.log(randomNumber)
+    setTimeout( () =>{
+        document.getElementById('loadingParticipante').style.display = 'none'
+        document.getElementById('main').style.display = 'none'
+        document.getElementById('participanteSorteado').style.display = 'block'
+        document.getElementById('numeroSorteado').innerText = randomNumber
+    }, 3000)
+}
+
+const nomeVencedor = (randomNumber) =>{
+    const dbClient = pegarLocalStorage()
+    dbClient.map(arr =>{
+        if(arr.numero == randomNumber && arr.numero > 0){
+            document.getElementById('nomeParticipante').innerText = `Parabéns, ${arr.nome}, hoje é o seu dia de sorte :). Você acaba de ganhar a rifa da simone!!! Entre em contato com ela para receber o seu prêmio. `
+            console.log(arr.nome)
+        }
+    })
+}
+
 btnSorteio.addEventListener('click', () =>{
     const dbClient = pegarLocalStorage()
-    console.log(dbClient)
     console.log(dbClient.length)
-    if(dbClient.length == 100){
-        console.log('Número de pessoas atingido')
+    if(hundred()){
+        const randomNumber = Math.floor(Math.random() * dbClient.length)
+        if(randomNumber == 0){
+            return
+        } else{
+            sorteioVencedor(randomNumber)
+            nomeVencedor(randomNumber)
+        }
+    } else {
+        alert('O número de contribuintes ainda não é o suficiente para realizar o sorteio')
     }
 })
-*/
 
+btnSortearNovamente.addEventListener('click', () =>{
+    document.getElementById('loadingParticipante').style.display = 'none'
+    document.getElementById('main').style.display = 'block'
+    document.getElementById('participanteSorteado').style.display = 'none'
+})
+
+
+hundred()
+colorTable()
